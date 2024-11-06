9e4a13a0627b73c75a5793c88a71e38cd08b087b
santerilukka <156800715+santerilukka@users.noreply.github.com>
2024-11-06 20:09:27 +0200

    Tehtävä 2

--- src/main/java/fi/utu/tech/assignment2/App2.java
+++ src/main/java/fi/utu/tech/assignment2/App2.java
@@ -11,6 +11,35 @@ import fi.utu.tech.common.SubmissionGenerator.Strategy;
 public class App2 {
     public static void main( String[] args )
     {
-        // Kopioi edellisen tehtävän ratkaisu tähän lähtökohdaksi
+        // Otetaan funktion aloitusaika talteen suoritusajan laskemista varten
+        long startTime = System.currentTimeMillis();
+
+        // Generoidaan kasa esimerkkitehtäväpalautuksia
+        List<Submission> ungradedSubmissions = SubmissionGenerator.generateSubmissions(21, 200, Strategy.STATIC);
+
+        // Tulostetaan tiedot esimerkkipalautuksista ennen arviointia
+        for (var ug : ungradedSubmissions) {
+            System.out.println(ug);
+        }
+
+        // Luodaan uusi arviointitehtävä
+        // arvioimattomilla palautuksilla
+        GradingTask gradingTask = new GradingTask(ungradedSubmissions);
+
+        // Luodaan uusi Thread-olio ja käynnistetään säie
+        Thread gradingThread = new Thread(gradingTask);
+        gradingThread.start();
+
+        // haetaan ja
+        // Tulostetaan arvioidut palautukset
+        List<Submission> gradedSubmissions = gradingTask.getGradedSubmissions();
+        System.out.println("------------ CUT HERE ------------");
+        for (var gs : gradedSubmissions) {
+            System.out.println(gs);
+        }
+
+        // Lasketaan funktion suoritusaika
+        System.out.printf("Total time for grading: %d ms%n", System.currentTimeMillis()-startTime);
+
     }
 }
